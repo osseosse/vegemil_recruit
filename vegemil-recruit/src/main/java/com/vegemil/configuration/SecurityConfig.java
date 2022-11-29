2105a5b5e046a8e4e3c48ac047b861d07836e016
kid4290 <kid4290@192.168.26.40>
2022-11-29 14:37:36 +0900

    시큐어 컨피그 분류 완료

--- vegemil-recruit/src/main/java/com/vegemil/configuration/SecurityConfig.java
+++ vegemil-recruit/src/main/java/com/vegemil/configuration/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,62 +25,94 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private MemberService memberService;
     
-    @Autowired
-    @Qualifier("dataSource")
-    private DataSource dataSource;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                //.antMatchers("/member/**").authenticated()
-                .antMatchers("/info/**").permitAll()
-            	.antMatchers("/member/login").permitAll()
-            	.antMatchers("/member/join").permitAll()
-            	.antMatchers("/member/register").permitAll()
-            	.antMatchers("/member/joinConfirm").permitAll()
-            	.antMatchers("/member/passwordReset").permitAll()
-            	.antMatchers("/mypage/qna").permitAll()
-                .antMatchers("/member/**").hasAuthority("USER") // user 권한의 유저만  접근가능
-                .antMatchers("/application/**").hasAuthority("USER") // user 권한의 유저만  접근가능
-                .antMatchers("/mypage/**").hasAuthority("USER") // user 권한의 유저만  접근가능
-                .antMatchers("/recruit/join", "/recruit/register").hasAuthority("USER") // user 권한의 유저만  접근가능
-                .antMatchers("/**").permitAll()
-            .anyRequest()
-                .authenticated()
-                .and().csrf().disable()
-            .formLogin()
-                .loginPage("/member/login")
-                .loginProcessingUrl("/member/loginProc")
-                .failureUrl("/member/login?error=true")
-                .defaultSuccessUrl("/")
-            .and()
-                .logout()
-	                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout")) // 로그아웃 시 URL 재정의
-	                .logoutSuccessUrl("/") // 로그아웃 성공 시 redirect 이동
-	                .invalidateHttpSession(true) //세션 삭제
-	                .deleteCookies("remember-me", "JSESSIONID") //자동 로그인 쿠키, Tomcat이 발급한 세션 유지 쿠키 삭제
-            .and()
-                .exceptionHandling()
-                .accessDeniedPage("/error/error")
-            .and()
-                .rememberMe()
-                .key("vegemil") //쿠키에 사용되는 값을 암호화하기 위한 키(key)값
-                .tokenRepository(persistentTokenRepository()) //DataSource 추가
-                .tokenValiditySeconds(604800); //토큰 유지 시간(초단위) - 일주일
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(memberService).passwordEncoder(new BCryptPasswordEncoder());
+    @Order(1)
+    @Configuration
+    public class AdminSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http
+	        	.antMatcher("/admin/**")
+	            	.authorizeRequests().anyRequest().hasAuthority("ADMIN")
+	            .and()
+	            	.csrf().disable()
+		            .formLogin()
+		                .loginPage("/member/adminLogin")
+		                .loginProcessingUrl("/admin/loginProc")
+		                .failureUrl("/admin/login?error=true")
+		                .defaultSuccessUrl("/")
+	            .and()
+	                .logout()
+		                .logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout")) // 로그아웃 시 URL 재정의
+		                .logoutSuccessUrl("/") // 로그아웃 성공 시 redirect 이동
+		                .invalidateHttpSession(true) // HTTP Session 초기화
+		                .deleteCookies("JSESSIONID") // 특정 쿠키 제거
+	            .and()
+	                .exceptionHandling()
+	                .accessDeniedPage("/accessDenied");
+	    }
+	    
+	    @Override
+	    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+	        auth.userDetailsService(memberService).passwordEncoder(new BCryptPasswordEncoder());
+	        //어드민으로 수정
+	        //auth.userDetailsService(adminService).passwordEncoder(new BCryptPasswordEncoder());
+	    }
+    
     }
     
-    // tokenRepository의 구현체
-    @Bean
-    public PersistentTokenRepository persistentTokenRepository() {
-    	JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
-    	repo.setDataSource(dataSource);
-    	return repo;
+    @Order(2)
+    @Configuration
+    public class MemberSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+	    @Autowired
+	    @Qualifier("dataSource")
+	    private DataSource dataSource;
+	
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http
+	            .authorizeRequests()
+	                .antMatchers("/application/**").hasAuthority("USER") // user 권한의 유저만  접근가능
+	                .antMatchers("/mypage/**").hasAuthority("USER") // user 권한의 유저만  접근가능
+	                .antMatchers("/recruit/join", "/recruit/register").hasAuthority("USER") // user 권한의 유저만  접근가능
+	            .anyRequest()
+	                .permitAll()
+	                .and().csrf().disable()
+	            .formLogin()
+	                .loginPage("/member/login")
+	                .loginProcessingUrl("/member/loginProc")
+	                .failureUrl("/member/login?error=true")
+	                .defaultSuccessUrl("/")
+	            .and()
+	                .logout()
+		                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout")) // 로그아웃 시 URL 재정의
+		                .logoutSuccessUrl("/") // 로그아웃 성공 시 redirect 이동
+		                .invalidateHttpSession(true) //세션 삭제
+		                .deleteCookies("remember-me", "JSESSIONID") //자동 로그인 쿠키, Tomcat이 발급한 세션 유지 쿠키 삭제
+	            .and()
+	                .exceptionHandling()
+	                .accessDeniedPage("/error/error")
+	            .and()
+	                .rememberMe()
+	                .key("vegemil") //쿠키에 사용되는 값을 암호화하기 위한 키(key)값
+	                .tokenRepository(persistentTokenRepository()) //DataSource 추가
+	                .tokenValiditySeconds(604800); //토큰 유지 시간(초단위) - 일주일
+	    }
+	
+	    @Override
+	    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+	        auth.userDetailsService(memberService).passwordEncoder(new BCryptPasswordEncoder());
+	    }
+	    
+	    // tokenRepository의 구현체
+	    @Bean
+	    public PersistentTokenRepository persistentTokenRepository() {
+	    	JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+	    	repo.setDataSource(dataSource);
+	    	return repo;
+	    }
+    
     }
 
 }
