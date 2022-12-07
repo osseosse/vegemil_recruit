e2a0c564cb1928c4c769501c1ed9d0675ddc322d
kid4290 <kid4290@192.168.26.40>
2022-12-07 14:53:23 +0900

    모집 내역 수정

--- vegemil-recruit/src/main/java/com/vegemil/configuration/DBConfiguration.java
+++ vegemil-recruit/src/main/java/com/vegemil/configuration/DBConfiguration.java
@@ -42,7 +42,7 @@ public class DBConfiguration {
 		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
 		factoryBean.setDataSource(dataSource());
 		factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mappers/**/*Mapper.xml"));
-		factoryBean.setTypeAliasesPackage("com.hyechun.*");
+		factoryBean.setTypeAliasesPackage("com.vegemil.*");
 		factoryBean.setConfiguration(mybatisConfg());
 		return factoryBean.getObject();
 	}
